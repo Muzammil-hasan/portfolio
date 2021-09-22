ff72df955af9dec8971cb9ad1f79455978e5e71b
Muzammil-hasan <muzammilsyed270300@gmail.com>
2021-09-22 18:22:24 +0530

    about page completed

--- dist/js/app.js
+++ dist/js/app.js
@@ -176,6 +176,58 @@ function ScrollToTop(_ref) {
 
 /***/ }),
 
+/***/ "./src/js/components/useViewport.jsx":
+/*!*******************************************!*\
+  !*** ./src/js/components/useViewport.jsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+
+
+var useViewport = function useViewport() {
+  var _React$useState = react__WEBPACK_IMPORTED_MODULE_0__.useState(window.innerWidth),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      width = _React$useState2[0],
+      setWidth = _React$useState2[1];
+
+  react__WEBPACK_IMPORTED_MODULE_0__.useEffect(function () {
+    var handleWindowResize = function handleWindowResize() {
+      return setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+    return function () {
+      return window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []); // Return the width so we can use it in our components
+
+  return {
+    width: width
+  };
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (useViewport);
+
+/***/ }),
+
 /***/ "./src/js/layout/Header.jsx":
 /*!**********************************!*\
   !*** ./src/js/layout/Header.jsx ***!
@@ -316,14 +368,91 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _layout_Header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../layout/Header */ "./src/js/layout/Header.jsx");
+/* harmony import */ var _components_useViewport__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/useViewport */ "./src/js/components/useViewport.jsx");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+
+
 
 
 
 var About = function About() {
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
-    children: "About page"
-  });
+  var _useViewport = (0,_components_useViewport__WEBPACK_IMPORTED_MODULE_2__.default)(),
+      width = _useViewport.width;
+
+  var breakpoints = 1050;
+
+  if (width > breakpoints) {
+    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("section", {
+        className: "about",
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+          className: "about__background"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+          className: "about__content",
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            className: "about__content-description",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            })
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            className: "about__content-image",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("img", {
+              src: "../../../dist/images/avatar.jpg",
+              alt: ""
+            })
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            className: "about__content-description --primary",
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("a", {
+              href: "https://muzam.ml/",
+              target: "_blank",
+              children: "Muzammil Hasan"
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            })]
+          })]
+        })]
+      })
+    });
+  } else if (width <= breakpoints) {
+    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("section", {
+        className: "about",
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+          className: "about__background"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+          className: "about__content",
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            className: "about__content-image",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("img", {
+              src: "../../../dist/images/avatar.jpg",
+              alt: ""
+            })
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            className: "about__content-description",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            })
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            className: "about__content-description --primary",
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("a", {
+              href: "https://muzam.ml/",
+              target: "_blank",
+              children: "Muzammil Hasan"
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("p", {
+              children: "I\u2019m a hard working and dedicated professional front-end developer with a keen eye for detail, and a determination to deliver the very highest quality. I take great pride in my work, and I always try to better myself with every project I work on."
+            })]
+          })]
+        })]
+      })
+    });
+  }
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (About);
--- src/js/pages/About.jsx
+++ src/js/pages/About.jsx
@@ -1,7 +1,96 @@
 import React from "react";
+import Header from "../layout/Header";
+import useViewport from "../components/useViewport";
 
 const About = () => {
-  return <div>About page</div>;
+  const { width } = useViewport();
+  const breakpoints = 1050;
+
+  if (width > breakpoints) {
+    return (
+      <>
+        {/* <Header /> */}
+        <section className='about'>
+          <div className='about__background'></div>
+
+          <div className='about__content'>
+            <div className='about__content-description'>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+            </div>
+
+            <div className='about__content-image'>
+              <img src='../../../dist/images/avatar.jpg' alt='' />
+            </div>
+
+            <div className='about__content-description --primary'>
+              <a href='https://muzam.ml/' target='_blank'>
+                Muzammil Hasan
+              </a>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  } else if (width <= breakpoints) {
+    return (
+      <>
+        {/* <Header /> */}
+        <section className='about'>
+          <div className='about__background'></div>
+
+          <div className='about__content'>
+            <div className='about__content-image'>
+              <img src='../../../dist/images/avatar.jpg' alt='' />
+            </div>
+
+            <div className='about__content-description'>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+            </div>
+
+            <div className='about__content-description --primary'>
+              <a href='https://muzam.ml/' target='_blank'>
+                Muzammil Hasan
+              </a>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+              <p>
+                I’m a hard working and dedicated professional front-end
+                developer with a keen eye for detail, and a determination to
+                deliver the very highest quality. I take great pride in my work,
+                and I always try to better myself with every project I work on.
+              </p>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 };
 
 export default About;
