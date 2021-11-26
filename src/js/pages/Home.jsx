7ecaf6adad766e554345da09ca204564b162d2b5
Muzammil-hasan <muzammilsyed270300@gmail.com>
2021-11-26 20:13:23 +0530

    Minor bug fixes

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -22,13 +22,13 @@ const Home = () => {
 							<line x1='4.37114e-08' y1='202.5' x2='733' y2='202.5' stroke='currentColor' />
 						</svg>
 					</div>
-					<p className='home__content-subtitle'>
-						front end developer <span>Based in Delhi</span>
-					</p>
 
 					<h1 className='home__content-title'>
 						Build your <span>ideas</span> into websites.
 					</h1>
+					<p className='home__content-subtitle'>
+						front end developer <span>Based in Delhi</span>
+					</p>
 
 					<div className='home__content-stroke second'>
 						<svg viewBox='0 0 733 1' fill='none' xmlns='http://www.w3.org/2000/svg'>
