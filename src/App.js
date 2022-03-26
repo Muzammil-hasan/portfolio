95e606b36bb8f00c0157108ec7c3b99111dc1dee
Muzammil <muzammilsyed270300@gmail.com>
2022-03-26 11:54:42 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -17,45 +17,45 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 
 function App() {
-	const [toggleTheme, setToggleTheme] = useState("");
+  const [toggleTheme, setToggleTheme] = useState("");
 
-	const darkTheme = "dark";
-	const lightTheme = "light";
-	let theme;
+  const darkTheme = "dark";
+  const lightTheme = "light";
+  let theme;
 
-	const switchTheme = (e) => {
-		if (theme === darkTheme) {
-			setToggleTheme(theme);
-			localStorage.setItem("theme", "light");
-			theme = lightTheme;
-		} else {
-			localStorage.setItem("theme", "dark");
-			setToggleTheme(theme);
-			theme = darkTheme;
-		}
-	};
+  const switchTheme = (e) => {
+    if (theme === darkTheme) {
+      setToggleTheme(theme);
+      localStorage.setItem("theme", "light");
+      theme = lightTheme;
+    } else {
+      localStorage.setItem("theme", "dark");
+      setToggleTheme(theme);
+      theme = darkTheme;
+    }
+  };
 
-	if (localStorage) {
-		theme = localStorage.getItem("theme");
-	}
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+  }
 
-	return (
-		<MouseContextProvider>
-			<main
-				className={`main ${theme == "light" ? "theme-light" : "theme-dark"}`}
-			>
-				<Preloader />
-				<Noise />
-				<Header switchTheme={switchTheme} theme={theme} />
-				<Cursor />
-				<Hero />
-				<About />
-				<Projects />
-				<Contact />
-				<Footer />
-			</main>
-		</MouseContextProvider>
-	);
+  return (
+    <MouseContextProvider>
+      <main
+        className={`main ${theme === "light" ? "theme-light" : "theme-dark"}`}
+      >
+        <Preloader />
+        <Noise />
+        <Header switchTheme={switchTheme} theme={theme} />
+        <Cursor />
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </main>
+    </MouseContextProvider>
+  );
 }
 
 export default App;
